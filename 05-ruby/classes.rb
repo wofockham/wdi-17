bb6ee8358d2cdd4cddb8193b2d0ed09676df7e5f
William of Ockham <certainthings@gmail.com>
2016-09-30 11:37:42 +1000

    class examples

--- 05-ruby/classes.rb
+++ 05-ruby/classes.rb
@@ -1,7 +1,20 @@
 require 'pry'
 
-class MarxBrother
+class Person
+  attr_accessor :eye_color, :date_of_birth
+
+  def mood
+    "I am happy"
+  end
+end
+
+class MarxBrother < Person # Inheritance
   attr_accessor :vice, :instrument # Define getters and setters for these properties
+
+  def initialize(instrument='singing', vice='burning down churches')
+    @instrument = instrument
+    @vice = vice
+  end
 
   def bio
     "I play the #{ @instrument } and I adore #{ @vice }."
@@ -9,18 +22,9 @@ class MarxBrother
 end
 
 groucho = MarxBrother.new
+
 harpo = MarxBrother.new
-
-
-
-# groucho = {
-#   :instrument => 'guitar',
-#   :vice => 'cigars'
-# }
-#
-# harpo = {
-#   :instrument => 'harp',
-#   :vice => 'mutism'
-# }
+harpo.vice = 'mutism'
+harpo.instrument = 'harp'
 
 binding.pry
