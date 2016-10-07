d00f4afa26030725f95fe5e3b0db302456d0a084
William of Ockham <certainthings@gmail.com>
2016-10-07 12:49:31 +1100

    user login

--- 08-rails/tunr/app/models/user.rb
+++ 08-rails/tunr/app/models/user.rb
@@ -2,13 +2,17 @@
 #
 # Table name: users
 #
-#  id         :integer          not null, primary key
-#  email      :text
-#  name       :text
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id              :integer          not null, primary key
+#  email           :text
+#  name            :text
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  password_digest :string
 #
 
 class User < ActiveRecord::Base
+  has_secure_password
+  validates :email, :presence => true, :uniqueness => true
+  # validates :name, :uniqueness => true, :length => {:minimum => 2}
   has_many :mixtapes
 end
