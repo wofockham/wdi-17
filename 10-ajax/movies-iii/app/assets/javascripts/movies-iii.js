d11850a475df9e7dcbb70c1e9eca76f41ceffd62
William of Ockham <certainthings@gmail.com>
2016-10-17 15:21:29 +1100

    ajax movies skellington

---
+++ 10-ajax/movies-iii/app/assets/javascripts/movies-iii.js
@@ -0,0 +1,13 @@
+var xhr = new XMLHttpRequest();
+
+$(document).ready(function () {
+  $('#movie_search').on('submit', function (e) {
+    e.preventDefault();
+
+    // Your code goes here
+
+    // Get the title entered by the user
+    // Request data on that title from omdb
+    // Update the page to show the poster
+  });
+});
