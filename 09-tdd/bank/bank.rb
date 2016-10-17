3d7b60bfd9a5beded73edb8a4c8da67ecf9f61fc
William of Ockham <certainthings@gmail.com>
2016-10-17 12:15:52 +1100

    TDD bank almost done

--- 09-tdd/bank/bank.rb
+++ 09-tdd/bank/bank.rb
@@ -1,4 +1,23 @@
 class Bank
+  attr_reader :name, :accounts
   def initialize(name)
+    @name = name
+    @accounts = Hash.new 0 # Default value so our mathematics doesn't break
+  end
+
+  def create_account(name, deposit)
+    @accounts[name] = deposit
+  end
+
+  def deposit(name, deposit)
+    @accounts[name] += deposit
+  end
+
+  def withdraw(name, amount)
+    @accounts[name] -= amount
+  end
+
+  def balance(name)
+    @accounts[name]
   end
 end
--- 09-tdd/bank/spec/bank_spec.rb
+++ 09-tdd/bank/spec/bank_spec.rb
@@ -1,13 +1,58 @@
 require_relative '../bank'
 describe Bank do
+  let(:bank) { Bank.new 'TDD Bank' }
+
   describe '.new' do
     it 'creates a new bank object' do
-      @bank = Bank.new 'TDD Bank' # Do something
-      expect(@bank).to_not eq nil # Assert that it worked
+      expect(bank).to_not eq nil # Assert that it worked
     end
 
     it 'has a name' do
-      expect(@bank.name).to eq 'TDD Bank'
+      expect(bank.name).to eq 'TDD Bank'
+    end
+
+    it 'has 0 accounts' do
+      expect(bank.accounts.count).to eq 0
+    end
+  end
+
+  describe '#create_account' do
+    it 'creates an account for some jerk with some starting deposit' do
+      bank.create_account 'Jonesy', 200
+      expect(bank.accounts['Jonesy']).to eq 200
+    end
+  end
+
+  describe '#deposit' do
+    it 'deposits an amount from the customer into their account' do
+      bank.create_account 'Craigsy', 200
+      bank.deposit 'Craigsy', 300
+      expect(bank.accounts['Craigsy']).to eq 500
+    end
+  end
+
+  describe '#withdraw' do
+    it 'withdraws an amount from a customer\'s account' do
+      bank.create_account 'Shazza', 200
+      bank.withdraw 'Shazza', 50
+      expect(bank.accounts['Shazza']).to eq 200-50
+    end
+
+    it 'ignores requests for withdrawals that exceed the balance' do
+      bank.create_account 'Chanel', 1
+      bank.withdraw 'Chanel', 5_000_000_000
+      expect(bank.balance('Chanel')).to eq 1
+    end
+
+    it 'is not an effective method of contraception' do
+      # Writing this test is an exercise for the reader
+    end
+  end
+
+  describe '#balance' do
+    it 'returns the current balance for a given account' do
+      bank.create_account 'Wayne', 1_000
+      expect(bank.balance('Wayne')).to eq 1_000
     end
   end
 end
