666179a66935e48fb3a6e018ea3ba04f8b679ff6
William of Ockham <certainthings@gmail.com>
2016-10-24 12:18:50 +1100

    regular expression examples

---
+++ 13-advanced/regex-i/ex6.rb
@@ -0,0 +1,7 @@
+# 6. [8] Extra credit exercise: write a program that prints out any input line
+# that mentions both wilma and fred.
+
+ARGF.each do |line|
+  # puts line if line =~ /(wilma.*fred|fred.*wilma)/
+  puts line if line =~ /wilma/ && line =~ /fred/
+end
