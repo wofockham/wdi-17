1cd595b469223e2c7b170a67f4d06092732028d0
William of Ockham <certainthings@gmail.com>
2016-11-09 12:43:14 +1100

    test for fruits#create

--- 09-tdd/fruitstore/spec/controllers/fruits_controller_spec.rb
+++ 09-tdd/fruitstore/spec/controllers/fruits_controller_spec.rb
@@ -45,4 +45,34 @@ RSpec.describe FruitsController, type: :controller do
       end
     end
   end
+
+  describe 'POST to create' do
+    describe 'a fruit with valid information' do
+      before do
+        post :create, { :fruit => { :name => 'Strawberry' } }
+      end
+
+      it 'should redirect to the show action' do
+        expect(response).to redirect_to(fruit_path(assigns(:fruit)))
+      end
+
+      it 'should increase the number of fruits' do
+        expect(Fruit.count).to eq(1)
+      end
+    end
+
+    describe 'a fruit invalid information' do
+      before do
+        post :create, { :fruit => { :name => '' } }
+      end
+
+      it 'should render the new template' do
+        expect(response).to render_template(:new)
+      end
+
+      it 'should not increase the number of fruits' do
+        expect(Fruit.count).to eq(0)
+      end
+    end
+  end
 end
