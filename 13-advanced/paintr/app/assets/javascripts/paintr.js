0e0f5f429886f5cb14250674eab428c2a9568f6d
William of Ockham <certainthings@gmail.com>
2016-11-11 12:24:09 +1100

    eat your face off, microsoft paint

---
+++ 13-advanced/paintr/app/assets/javascripts/paintr.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+  // Adds a swatch with the user's colour choice to the palette.
+  $('#add_color').on('click', function () {
+    var color = $('#color').val();
+    var $swatch = $('<div class="swatch"></div>').css('background-color', color);
+    $swatch.prependTo('#palette');
+  });
+
+  // Make the selected swatch the active color.
+  $('#palette').on('click', '.swatch', function () { // Event delegation
+    $('.selected').removeClass('selected');
+    $(this).addClass('selected');
+  });
+
+  for (var i = 0; i < 5000; i++) {
+    $('#canvas').append('<div class="pixel"/>');
+  }
+
+  // Paint pixels on mouseover.
+  $('.pixel').on('mouseover', function () {
+    var color = $('.selected').css('background-color');
+    $(this).css('background-color', color);
+  });
+});
---
+++ 13-advanced/paintr/app/controllers/pages_controller.rb
@@ -0,0 +1,7 @@
+class PagesController < ApplicationController
+  def hamlintro
+  end
+
+  def canvas
+  end
+end
