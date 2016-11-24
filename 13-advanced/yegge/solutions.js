3e242053a99ac1bce4525a8e713902dadef356ea
William of Ockham <certainthings@gmail.com>
2016-11-24 12:30:10 +1100

    fibonacci, multiplication tables

--- 13-advanced/yegge/solutions.js
+++ 13-advanced/yegge/solutions.js
@@ -22,8 +22,44 @@ var reverse = function (s) { // Fancy recursive version.
 
 console.log('reverse: ', reverse('god is a hotdog'));
 
+////////////////////////////////////////////////////////////////////////////////
 // Write function to compute Nth fibonacci number:
+var fibonacci = function (n) { // Iterative version.
+  var a = b = 1; // We already know the first 2 fibonacci values are both 1.
+  for (var i = 3; i <= n; i++) {
+    var newB = a + b;
+    a = b;
+    b = newB;
+  }
+  return b;
+}
+
+var fibonacci = function (n) { // Slow recursive version.
+  if (n <= 2) {
+    return 1; // Base case
+  } else {
+    return fibonacci(n - 1) + fibonacci(n - 2);
+  }
+}
+
+// Fast fibonacci is left as an exercise to the reader.
+
+console.log('fibonacci', fibonacci(7)); // Expect 13
+
+////////////////////////////////////////////////////////////////////////////////
 // Print out the grade-school multiplication table up to 12x12
+var twelveTimesTables = function () {
+  for (var i = 1; i <= 12; i++) {
+    for (var j = 1; j <= 12; j++) {
+      // You might like to pad the output so it lines up neatly.
+      console.log(i + ' x ' + j + ' = ' + (i*j));
+    }
+  }
+}
+
+console.log('multiplication');
+twelveTimesTables();
+
 // Write a function that sums up integers from a text file, one int per line.
 // Write function to print the odd numbers from 1 to 99.
 // Find the largest int value in an int array.
